1cac4708f8507852e7fbd39d942aba1819b59691
geethikagrace <geethika2219@gmail.com>
2023-05-10 12:01:12 -0700

    ansible roboshop structure setup

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "sonydevops.22"
+key    = "parameter-store/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "parameters" {
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
+  type   = "String"
+  key_id = "b24b0646-42c1-422c-ba99-bb9db96b3637"
+}
