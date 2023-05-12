82f5d929c9d0cd1b4f628c8434ed83d860e06290
geethikagrace <geethika2219@gmail.com>
2023-05-12 10:40:48 -0700

    lookup

--- main.tf
+++ main.tf
@@ -3,5 +3,15 @@ resource "aws_ssm_parameter" "parameters" {
   name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
   type   = "String"
-  key_id = "b24b0646-42c1-422c-ba99-bb9db96b3637"
+  key_id = "dce90622-5a23-4f82-a639-be841f534702"
+}
+
+## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
+## One of the scenario that mostly used in real way is , Enter the passwords manually in Parameter Store by someone for passwords.
+resource "aws_ssm_parameter" "passwords" {
+  count  = length(var.passwords)
+  name   = var.passwords[count.index].name
+  value  = var.passwords[count.index].value
+  type   = "SecureString"
+  key_id = "dce90622-5a23-4f82-a639-be841f534702"
 }
