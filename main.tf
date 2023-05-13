1c6514a928e0e956d36481767732b0705963b41f
geethikagrace <geethika2219@gmail.com>
2023-05-13 14:07:15 -0700

    lookup

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameters" {
   name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
   type   = "String"
-  key_id = "dce90622-5a23-4f82-a639-be841f534702"
+  key_id = "b24b0646-42c1-422c-ba99-bb9db96b3637"
 }
 
 ## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
@@ -13,5 +13,6 @@ resource "aws_ssm_parameter" "passwords" {
   name   = var.passwords[count.index].name
   value  = var.passwords[count.index].value
   type   = "SecureString"
-  key_id = "dce90622-5a23-4f82-a639-be841f534702"
+  key_id = "b24b0646-42c1-422c-ba99-bb9db96b3637"
 }
+
